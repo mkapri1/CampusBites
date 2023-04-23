b45ce8d81161ff724c5fc24f2aae72efeeb8099e
Malika Kapri <mkapri1@student.gsu.edu>
2023-04-23 14:17:48 -0400

    homePage

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,33 @@
+const cardGrid = document.querySelector('.card-grid');
+            const loadMoreButton = document.querySelector('.load-more-button');
+            const cardsPerLoad = 2;
+            let currentCardIndex = 3;
+
+            // Show only the first 3 cards on page load
+            const cards = document.querySelectorAll('.card');
+            for (let i = 0; i < 10; i++) {
+            if (i < 3) {
+                cards[i].style.display = 'block';
+            } else {
+                cards[i].style.display = 'none';
+            }
+            }
+
+            loadMoreButton.addEventListener('click', () => {
+            // Show the next set of cards
+            for (let i = currentCardIndex; i < currentCardIndex + cardsPerLoad && i < 10; i++) {
+                cards[i].style.display = 'block';
+            }
+            
+            // Update the current card index
+            currentCardIndex += cardsPerLoad;
+            
+            // Hide the "Load More" button if there are no more cards to load
+            if (currentCardIndex >= 10) {
+                loadMoreButton.style.display = 'none';
+            }
+            });
+
 function initMap() {
     var location = { lat: 33.755451, lng: -84.389352 }; // set the location to San Francisco
     var map = new google.maps.Map(document.getElementById("gmap"), {

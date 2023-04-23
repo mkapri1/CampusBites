e5739ecd61a68a8df124f6c149bb3f1b1c4f1b4b
Malika Kapri <mkapri1@student.gsu.edu>
2023-04-23 18:56:43 -0400

    map

--- frontend/script.js
+++ frontend/script.js
@@ -28,16 +28,20 @@ const cardGrid = document.querySelector('.card-grid');
             }
             });
 
+let map;
 function initMap() {
-    var location = { lat: 33.755451, lng: -84.389352 }; // set the location to San Francisco
-    var map = new google.maps.Map(document.getElementById("gmap"), {
-      zoom: 14,
-      center: location,
-    });
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map,
-    });
+     map = new google.maps.Map(document.getElementById("map"),{
+      center: { lat: 33.755451, lng: -84.389352},
+      zoom: 6
+     });
+     new google.maps.Marker({
+        position: { lat: 33.755451, lng: -84.389352},
+        map: map,
+        label: "A",
+        title: "Moge Tee",
+        draggable: false,
+        animation: google.maps.Animation.BOUNCE,
+     })
   }
   
 var slideIndex = 1;
@@ -56,3 +60,98 @@ var slideIndex = 1;
         slides[slideIndex - 1].style.display = "block";
         setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+function reviews(){
+    //Examples
+  let reviews = [
+    { name: "Carlos",rating: 4, comment: "Great products!" },
+    { name: "Carlos",rating: 5, comment: "I love it!" },
+    { name: "Carlos",rating: 3, comment: "Not bad" }
+  ];
+
+//Calculate Averages
+let totalRating = 0;
+reviews.forEach((review) => {
+  totalRating += review.rating;
+});
+const averageRating = totalRating / reviews.length;
+
+// Update the average rating in the HTML
+const averageSpan = document.querySelector('#average');
+averageSpan.textContent = averageRating.toFixed(1);
+
+// Display the existing reviews
+const reviewsUL = document.querySelector('#reviews ul');
+reviews.forEach((review) => {
+  const li = document.createElement('li');
+  li.textContent = `${review.rating} stars - ${review.comment}`;
+  reviewsUL.appendChild(li);
+});
+
+// Refresh New Form Submission Input
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  // Get the user's rating and comment
+  const ratingInput = document.querySelector('#rating');
+  const rating = parseInt(ratingInput.value);
+  const commentInput = document.querySelector('#comment');
+  const comment = commentInput.value;
+  
+  // Add the new review to the array
+  reviews.push({ rating: rating, comment: comment });
+  
+  // Create a new review element
+  const newReview = document.createElement('li');
+  newReview.textContent = `${rating} stars - ${comment}`;
+  
+  // Add the new review to the list
+  reviewsUL.appendChild(newReview);
+  
+  // Calculate the new average rating
+  const newTotalRating = totalRating + rating;
+  const newAverageRating = newTotalRating / reviews.length;
+  
+  // Update the average rating in the HTML
+  averageSpan.textContent = newAverageRating.toFixed(1);
+  
+  // Reset the form
+  ratingInput.value = '';
+  commentInput.value = '';
+});
+}
+
+function addComment() {
+// inputs
+var name = document.getElementById("name").value;
+var comment = document.getElementById("comment").value;
+
+// create comment element
+var commentElement = document.createElement("div");
+commentElement.classList.add("comment");
+commentElement.innerHTML = "<h3>" + name + "</h3><p>" + comment + "</p>";
+
+var commentsSection = document.getElementById("comments");
+commentsSection.appendChild(commentElement);
+
+// save comment in localStorage
+var comments = JSON.parse(localStorage.getItem("comments")) || [];
+comments.push({ name: name, comment: comment });
+localStorage.setItem("comments", JSON.stringify(comments));
+
+document.getElementById("name").value = "";
+document.getElementById("comment").value = "";
+}
+
+// retrieve comments from localStorage on page load
+window.onload = function() {
+var comments = JSON.parse(localStorage.getItem("comments")) || [];
+var commentsSection = document.getElementById("comments");
+comments.forEach(function(comment) {
+var commentElement = document.createElement("div");
+commentElement.classList.add("comment");
+commentElement.innerHTML = "<h3>" + comment.name + "</h3><p>" + comment.comment + "</p>";
+commentsSection.appendChild(commentElement);
+});
+};

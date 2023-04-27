f830793791825d8042bc168d05f90ba5c9b55b6c
razsa <30312028+razsa@users.noreply.github.com>
2023-04-27 17:19:43 -0400

    connection strings and things

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
+#Creating an API REST server
+    # - Select, Update, Delete, Modify and Insert operations (MySQL database)
+
+#Requirements
+    #-Python (3.7 and above)
+    #-XAMPP (Apache + MySQL installed)
+    #-Python modules
+        #-Flask
+        #-Jsonify
+        #-Flask-restful
+        #-Flask-mysql
+        #-PyMySQL
+        #-Flask-cors
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
